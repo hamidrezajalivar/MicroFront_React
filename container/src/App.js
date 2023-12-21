3f04675eb86b87be84392eee5b3d101914483422
hamidrezajalivar <hamidrezajalivar17830@gmail.com>
2023-12-21 22:57:37 +0330

    History

---
+++ container/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MarketingApp from "./component/MarketingApp";
+import Header from './component/Header'
+import {BrowserRouter} from "react-router-dom";
+
+export default () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <MarketingApp/>
+            </div>
+        </BrowserRouter>
+    )
+}
